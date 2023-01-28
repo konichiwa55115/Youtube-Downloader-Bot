897aef454fba1cf8de628af27d46a758dfab66cd
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-01-28 02:04:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import  os
 
 BOT_TOKEN = "5938308636:AAFIcmKrO7167RQq21eFhCCG3NrTE0DKOoI"
-APP_ID = "17983098"
+APP_ID = int(17983098)
 API_HASH = "ee28199396e0925f1f44d945ac174f64"
 
 youtube_next_fetch = 0  # time in minute
